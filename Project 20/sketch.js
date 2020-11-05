44acf4a76852aac260436f485b5987e67d2603dc
ParshvaJogani <69082393+ParshvaJogani@users.noreply.github.com>
2020-11-05 20:16:17 +0530

    Update sketch.js

--- Project 20/sketch.js
+++ Project 20/sketch.js
@@ -4,9 +4,9 @@ var speed,weight;
 
 function setup() {
   createCanvas(1600,400);
+ thick = random(22,83)
  
- 
- wall = createSprite(1500,200,60,height/2);
+ wall = createSprite(1500,200,thick,height/2);
  wall.shapeColor=color(80,80,80);
  car = createSprite(50,200,50,50);
  
